3dab71c1080121cb95cf20ae1ac8c405f56006e8
Quin Kennedy <quinkennedy@gmail.com>
2015-12-06 16:44:55 -0800

    quick beginning to pseudo code for manfred mohr piece

--- bin/data/manfredMohrP196A/exampleCode.cpp
+++ bin/data/manfredMohrP196A/exampleCode.cpp
@@ -1,1 +1,28 @@
-// code goes here!
+draw(){
+	pushMatrix();
+	scale([[scale]], [[scale]], [[scale]]);
+	for each row{
+		for each column{
+			drawComposition(row, column);
+		}
+	}
+	popMatrix();
+}
+
+drawComposition(int row, int column){
+	pushMatrix();
+	translate([[spacing]] * column, [[spacing]] * row, 0);
+	drawLine(0, -[[spacing]]/2, 0, [[spacing]]/2);
+	glScissor(screenSpaceRight, screenSpaceBottom, screenSpaceWidth, screenSpaceHeight/2);
+	drawCube([[cubeARotationX]], [[cubeARotationY]], [[cubeARotationZ]]);
+	glScissor(screenSpaceRight, screenSpaceBottom + screenSpaceHeight/2, screenSpaceWidth, screenSpaceHeight/2);
+	drawCube([[cubeBRotationX]], [[cubeBRotationY]], [[cubeBRotationZ]]);
+	popMatrix();
+}
+
+drawCube(float rotationX, rotationY, rotationZ){
+	pushMatrix();
+	rotate(rotationX, rotationY, rotationZ);
+	drawUnitCube();
+	popMatrix();
+}
