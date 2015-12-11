edf227d1419a5da38b3bf1c04dd674368bbb2c58
Roy Macdonald <macdonald.roy@gmail.com>
2015-12-11 15:39:01 -0500

    Changed VISUALS_WIDTH and VISUALS_HEIGHT to 504

--- src/scenes/appConstants.h
+++ src/scenes/appConstants.h
@@ -4,8 +4,8 @@
 // assume that visuals and code are the same here....
 // and also assuming our screen is square :)
 
-#define VISUALS_WIDTH 1000
-#define VISUALS_HEIGHT 1000
+#define VISUALS_WIDTH 504
+#define VISUALS_HEIGHT 504
 
 #define CODE_WIDTH VISUALS_WIDTH
 #define CODE_HEIGHT VISUALS_HEIGHT
