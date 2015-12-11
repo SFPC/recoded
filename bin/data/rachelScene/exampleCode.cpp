8351c5a24061072252f9789d8a2c04a080793a05
Chris Anderson <chanderson0@gmail.com>
2015-12-11 14:16:15 -0500

    cleaner rachelScene pseudocode

--- bin/data/rachelScene/exampleCode.cpp
+++ bin/data/rachelScene/exampleCode.cpp
@@ -1,15 +1,9 @@
-void main(void) {
-    float x = gl_FragCoord.x;
-    float y = gl_FragCoord.y;
-    
-    float mx = [[posX]] / x;
-    float my = [[posY]] / y;
-    
+void drawPixel(int x, int y) {
     // Make it weird
-    float factor_one = [[time]] * y * sin(y * 0.01) * my;
-    float factor_two = mx + my;
+    float factor_one = [[time]] * y * sin(y) * [[posY]] / y;
+    float factor_two = [[posX]] / x + [[posY]] / y;
     float weirdness = tan(factor_one / factor_two);
     
     // Output color
-    gl_FragColor = vec4(weirdness, weirdness, weirdness, 1.0);
+    setPixelBrightness(weirdness);
 }
