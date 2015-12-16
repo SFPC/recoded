26498740c6710bf93470a66d0d451568053bdd46
Chris Anderson <chanderson0@gmail.com>
2015-12-16 13:12:36 -0600

    update loloWhitney to have less distortion, simple pseudocode

--- bin/data/loloWhitney/exampleCode.cpp
+++ bin/data/loloWhitney/exampleCode.cpp
@@ -1,23 +1,12 @@
-
-shader_rip.begin();
-shader_rip.setUniform1f("disor", [[Disort]]);
-shader_rip.setUniform1f("wave_am", [[Wave Size]]);
-
-
-ofPushMatrix();
-    ofTranslate(center.x,center.y);
-    ofNoFill();
-    for (int i=0; i<q_main_val.size(); i++) {
-        ofFloatColor _color = ofFloatColor(255);
-        _color.setHsb(ofMap(i, 0, q_main_val.size(), 0.0, 1.0), 1.0, 1.0);
-        float   _size = sin(q_main_val.at(i));
-        ofRotateZ(q_main_val.at(i)*[[Rotation Val]]);
-        ofSetColor(_color);
-        float   _noi = ofNoise(q_main_val.at(i)*.1,ofGetFrameNum()*.01);
-        _noi*=100.0;
+void draw() {
+    for (int i = 0; i < ellipses.size; i++) {
+        ellipse = ellipses[i];
+        setColor(ellipse.color);
+        setRotation(ellipse.rotation);
         
-        ofDrawEllipse(q_main_val.at(i)*.2)*100.0, (q_main_val.at(i)*.2)*100.0, _size*ellipse_size/2.0, _size*[[Ellipse Size]]);
+        setDistortion([[Disort]]);
+        setWaveSize([[Wave Size]]);
+        scale([[Ellipse Size]]);
+        ellipse.draw();
     }
- ofPopMatrix();
-
- shader_rip.end();
+}
