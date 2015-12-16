5222c7e93ade7a10838c7d7a25c46dd8570f1051
Chris Anderson <chanderson0@gmail.com>
2015-12-16 15:00:02 -0600

    better rodrigoBelfort pseudocode

--- bin/data/rodrigoBelfort/exampleCode.cpp
+++ bin/data/rodrigoBelfort/exampleCode.cpp
@@ -1,32 +1,24 @@
-void belfort::update() {
-    float time = ofGetElapsedTimef();
-    
-    if (lastLineTime + [[timeBtwnLines]] < time) {
-        lastLineTime = time;
+void update() {
+    if (timeSinceLine < [[timeBtwnLines]]) {
         // Copy all the lines down one
-        for (int i = BELFORT_ROWS - 1; i >= 1; i--) {
-            memcpy(dots[i], dots[i-1], sizeof(int) * BELFORT_COLS);
+        for (int i = ROWS - 1; i >= 1; i--) {
+            copyLine(i, i-1);
         }
         
         // Sometimes randomly replace the first line
-        if (ofRandomuf() < [[pNewLine]]) {
-            for (int i = 0; i < BELFORT_COLS; i++) {
-                dots[0][i] = round(ofRandomuf());
-            }
+        if (random() < [[pNewLine]]) {
+            randomLine();
         }
     }
 }
 
-void belfort::draw(){
-    for (int i = 0; i < BELFORT_ROWS; i++) {
-        for (int j = 0; j < BELFORT_COLS; j++) {
+void draw(){
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            // Skip if there's no rectangle to draw
             if (!dots[i][j]) continue;
-            
-            ofFill();
-            ofSetLineWidth(0);
-            ofDrawRectangle(j*(rectSize+paddingSizeX)+WINDOW_PADDING,
-                            i*(rectSize+paddingSizeY)+WINDOW_PADDING,
-                            rectSize, rectSize);
+
+            ofDrawRectangle(i, j, rectSize);
         }
     }
 }
