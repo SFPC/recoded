f392e80bd391bca74e5120b34038e3c599b354d3
Chris Anderson <chanderson0@gmail.com>
2015-12-17 16:20:55 -0600

    update loloVera2 to be more controllable

--- src/scenes/loloVera2/loloVera2.cpp
+++ src/scenes/loloVera2/loloVera2.cpp
@@ -7,12 +7,14 @@ void loloVera2::setup(){
 //    param.set("param", 5, 0, 100);
 //    parameters.add(param);
 
-    random_factor.set("RANDOM FACTOR",16.0,1.5,20.0);
+    random_factor.set("RANDOM FACTOR",1.0,0.0,30.0);
     u_bang.set("UPDATE PATHS",21,2,30);
     line_width.set("LINE WEIGHT",2.0,1.0,5.0);
+    redSpeed.set("redSpeed",9,0,10);
     parameters.add(random_factor);
     parameters.add(u_bang);
     parameters.add(line_width);
+    parameters.add(redSpeed);
     
     texture.load("scenes/loloVera2/tex.jpg");
     
@@ -40,19 +42,20 @@ void loloVera2::update(){
      
     for (int i=0;i<vera_paths.size(); i++) {
         vera_paths.at(i).l_width = line_width;
-        vera_paths.at(i).update(random_factor,u_bang);
+        vera_paths.at(i).update(random_factor,u_bang, redSpeed);
     }
     
 }
 
 void loloVera2::draw(){
     
-    ofBackground(200);
+    //ofBackground(200);
+    ofBackground(0);
     ofPushStyle();
-    ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
-    texture.draw(0.0, 0.0);
+    //ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
+    //texture.draw(0.0, 0.0);
     
-    ofDisableBlendMode();
+    //ofDisableBlendMode();
    
     for (int i=1;i<vera_paths.size(); i++) {
         float d = ofDist(vera_paths.at(i-1).d_guide.x, vera_paths.at(i-1).d_guide.y, vera_paths.at(i).d_guide.x, vera_paths.at(i).d_guide.y);
@@ -61,7 +64,7 @@ void loloVera2::draw(){
         ofDrawLine(vera_paths.at(i-1).d_guide.x,vera_paths.at(i-1).d_guide.y,vera_paths.at(i).d_guide.x,vera_paths.at(i).d_guide.y);
     }
     
-    ofEnableAlphaBlending();
+    //ofEnableAlphaBlending();
     for (int i=0;i<vera_paths.size()-1; i++) {
         for(int j=0;j<vera_paths.at(i).q_guide.size();j++)
         {
@@ -71,13 +74,13 @@ void loloVera2::draw(){
                 float   d_points = p0.distance(p1);
                 if(d_points < size_bloq){
                     float   factor_d_points = ofMap(d_points, 0.0, size_bloq*2, 1.0, 0.0,true);
-                    ofSetColor(0.0,factor_d_points*100);
+                    ofSetColor(255,factor_d_points*100);
                     ofDrawLine(p0, p1);
                 }
             }
         }
     }
-    ofDisableAlphaBlending();
+    //ofDisableAlphaBlending();
 
     
     for (int i=0;i<vera_paths.size(); i++) {
