d6c2e8bba69ae52480e64ce8ae59848c2194002f
Roy Macdonald <macdonald.roy@gmail.com>
2015-12-17 23:15:20 -0600

    added sketch sent by Caterina Lee & Joao Freire

--- src/scenes/RileyRemake-Catarina_Lee__Joao_Freire/RileyRemake-Catarina_Lee__Joao_Freire.cpp
+++ src/scenes/RileyRemake-Catarina_Lee__Joao_Freire/RileyRemake-Catarina_Lee__Joao_Freire.cpp
@@ -1,18 +1,89 @@
 
 #include "RileyRemake-Catarina_Lee__Joao_Freire.h"
-
-void RileyRemake-Catarina_Lee__Joao_Freire::setup(){
+#define NUM_COLORS 5
+void RileyRemake_Catarina_Lee__Joao_Freire::setup(){
   
 // setup pramaters
 //    param.set("param", 5, 0, 100);
 //    parameters.add(param);
+    
+    ofEnableAlphaBlending();
+    
+    for(int i = 0; i < 7; i++)
+    {
+        colors.push_back(ofParameter<ofColor>());
+        parameters.add(colors.back().set("color"+ofToString(i), ofColor(100,100,140), ofColor(0,0),ofColor(255,255)));
+    }
+    
+    for(int j = 0; j < 18; j++)
+    {
+        ofColor auxColor = colors[ofRandom(0,7)];
+        
+        stripe1.clear();
+        for(int i = 0; i < 7; i ++)
+        {
+            vertical_stripe *aux;
+            aux = new vertical_stripe();
+            aux->create(j * 60, -j*30+ i*7*30, auxColor);
+            stripe1.push_back(aux);
+        }
+        stripes.push_back(stripe1);
+    }
+    
+    int nFloating = ofRandom(400,500);
+    cell aux;
+    
+    for(int i = 0; i < nFloating; i++)
+    {
+        int x = ofRandom(17);
+        int y = ofRandom(4);
+        int num_cell = ofRandom(7);
+        aux.create(stripes[x][y]->cells[num_cell]->origin.x + 1*17*60,  stripes[x][y]->cells[num_cell]->origin.y-30*30,"diagonal", colors[ofRandom(7)]);
+        floating_cells.push_back(aux);
+    }
+    
 
     loadCode("scenes/RileyRemake-Catarina_Lee__Joao_Freire/exampleCode.cpp");
 }
 
-void RileyRemake-Catarina_Lee__Joao_Freire::update(){
+void RileyRemake_Catarina_Lee__Joao_Freire::update(){
+    ofBackground(0, 0, 0);
+    for(int j = 0; j < stripes.size(); j++)
+    {
+        for(int i = 0; i < stripes[j].size(); i ++)
+        {
+            stripes[j][i]->update();
+            if(stripes[j][i]->cells[0]->origin.y > ofGetHeight() + stripes[j][i]->cells[0]->size.y * .8)
+            {
+                stripes[j][i]->moveUp(stripes[j][i]->cells[0]->origin.y - 6*7*30);
+                stripes[j][i]->color = colors[ofRandom(0,7)];
+            }
+        }
+    }
     
+    for(int i = 0; i < floating_cells.size(); i++)
+    {
+        floating_cells[i].update();
+        if(floating_cells[i].origin.y > ofGetHeight() + floating_cells[i].size.y || floating_cells[i].origin.x < -60)
+        {
+            int x = ofRandom(20);
+            int y = ofRandom(15);
+            int num_cell = ofRandom(7);
+            floating_cells[i].origin += ofPoint((18 + x)*60, -(6 + y)*7*30);
+            floating_cells[i].color = colors[ofRandom(0,7)];
+            
+        }
+    }
 }
 
-void RileyRemake-Catarina_Lee__Joao_Freire::draw(){
+void RileyRemake_Catarina_Lee__Joao_Freire::draw(){
+    for(int j = 0; j < stripes.size(); j++){
+        for(int i = 0; i < stripes[j].size(); i++)
+        {
+            stripes[j][i]->draw();
+        }
+    }
+    for(int i = 0; i < floating_cells.size(); i++){
+        floating_cells[i].draw();
+    }
 }
--- src/scenes/RileyRemake-Catarina_Lee__Joao_Freire/RileyRemake-Catarina_Lee__Joao_Freire.h
+++ src/scenes/RileyRemake-Catarina_Lee__Joao_Freire/RileyRemake-Catarina_Lee__Joao_Freire.h
@@ -3,13 +3,116 @@
 #include "ofMain.h"
 #include "baseScene.h"
 
-class RileyRemake-Catarina_Lee__Joao_Freire : public baseScene {
+
+struct cell
+{
+    void create(int _x, int _y, string _direction, ofColor _color)
+    {
+        origin.set(_x, _y);
+        size.set(60,30);
+        direction = _direction;
+        color = _color;
+    }
+    
+    void draw()
+    {
+        ofFill();
+        ofSetColor(color.r, color.g, color.b, color.a);
+        
+        glBegin(GL_QUADS) ;
+        ofPoint aux;
+        
+        
+        glVertex2f(origin.x, origin.y) ;
+        aux.x = origin.x + size.x;
+        aux.y = origin.y - size.y ;
+        glVertex2f(aux.x, aux.y) ;
+        aux += ofPoint(0,size.y);
+        glVertex2f(aux.x, aux.y) ;
+        aux += ofPoint(-size.x,  size.y);
+        glVertex2f(aux.x, aux.y) ;
+        glEnd() ;
+    }
+    
+    void update()
+    {
+        if(direction == "down")
+        {
+            origin.y += 1;
+        }
+        else
+        {
+            origin.y += 1.5;
+            origin.x -= 1;
+        }
+    }
+    
+    ofPoint origin;
+    ofPoint size;
+    string direction;
+    ofColor color;
+    
+};
+
+struct vertical_stripe
+{
+    void create(int _x, int _y, ofColor _color)
+    {
+        color = _color;
+        
+        for(int i = 0; i < 7; i++)
+        {
+            cell* aux;
+            aux = new cell();
+            aux->create(_x, _y + i*30, "down", color);
+            cells.push_back(aux);
+        }
+    }
+    
+    void moveUp(int _y)
+    {
+        cells[0]->origin.y = _y;
+        for (int i = 0; i < cells.size(); i++)
+        {
+            cells[i]->origin.y = cells[0]->origin.y + i*30;
+        }
+    }
+    
+    void update()
+    {
+        for(int i = 0; i < cells.size(); i++)
+        {
+            cells[i]->color = color;
+            cells[i]->update();
+        }
+    }
+    
+    void draw()
+    {
+        for(int i = 0; i < cells.size(); i++)
+        {
+            cells[i]->draw();
+        }
+    }
+    
+    vector<cell*> cells;
+    ofColor color;
+};
+
+
+class RileyRemake_Catarina_Lee__Joao_Freire : public baseScene {
     
 public:
     
     void setup();
     void update();
     void draw();
+    vector <ofParameter<ofColor> > colors;
     
+    vector<vertical_stripe*> stripe1, stripe2, stripe3, stripe4;
+    
+    vector< vector <vertical_stripe*> > stripes;
+    
+    vector<cell> floating_cells;
 //    ofParameter<float> param;
 };
