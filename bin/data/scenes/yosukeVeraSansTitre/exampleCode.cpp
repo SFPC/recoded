0f34e543b66213bee921e72f0d56142110c27cbe
Chris Anderson <chanderson0@gmail.com>
2015-12-18 22:27:04 -0600

    update yosukeVeraSansTitre pseudocode and param ranges

--- bin/data/scenes/yosukeVeraSansTitre/exampleCode.cpp
+++ bin/data/scenes/yosukeVeraSansTitre/exampleCode.cpp
@@ -1,12 +1,12 @@
-void veraSansTitre::draw(){
-  for (int i = 0; i < [[number-of-row]]; i++) {
-    for (int j = 0; j< [[number-of-colmn]]; j++) {
+void draw() {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 20; j++) {
       // Add spacing
       translate([[space-x]], [[space-y]]);
 
       // Get a random value with a seed
       float randomValue = random([[displacementZ]]);
-        
+
       // Add random movement only in Y axis
       translate(0, [[displacement-coef]] * randomValue);
       drawRectangle(i, j, rectWidth, rectHeight);
