06caec76551fde85057daddd586dcfcaefc9e287
Roy Macdonald <macdonald.roy@gmail.com>
2015-12-19 12:15:58 -0600

    fixed merge conflict automerge

--- src/scenes/sceneManager.cpp
+++ src/scenes/sceneManager.cpp
@@ -349,12 +349,12 @@ void sceneManager::update(){
 
     if (shouldDrawScene) {
 #ifdef USE_MIDI_PARAM_SYNC
-        if(scenes[currentScene]->bAnimateScene){
-            if (isMidiConnected)
-                scenes[currentScene]->updateMidiParams();
+        if (enableMidiUpdate.get()){
+            if(scenes[currentScene]->bAnimateScene){
+                if (isMidiConnected)
+                    scenes[currentScene]->updateMidiParams();
+            }
         }
-        if (enableMidiUpdate.get())
-            scenes[currentScene]->updateMidiParams();
 #endif
         scenes[currentScene]->update();
     }
