928f445b3dfd07cd2c334b52b6c70d100af0f7d2
mgs <mgsimpson@gmail.com>
2015-12-19 04:45:51 -0600

    Revised pseudocode fro RileyHiddenSquares scene

--- bin/data/scenes/mgsRileyEllipsesAndSquares/exampleCode.cpp
+++ bin/data/scenes/mgsRileyEllipsesAndSquares/exampleCode.cpp
@@ -1,17 +1,42 @@
 // Bridget Riley - Hidden Squares
-for (float x = 0; x < dimensions.width; x+=unitSize) {
-  for (float y = 0; y < dimensions.height; y+=unitSize) {
-    if (x/unitSize % 2 == 0 && y/unitSize % 2 == 0) {
-      if ((x > px0 && x < px1 && y > py0 && y < py1)  ||
-          (x > px2 && x < px3 && y > py2 && y < py3)  ||
-          (x > px4 && x < px5 && y > py4 && y < py5)  ||
-          (x > px6 && x < px7 && y > py6 && y < py7)) {
-        ofDrawRectangle(x+unitSize, y+unitSize, unitSize, unitSize);
-        ofDrawRectangle(x, y, unitSize, unitSize);
-      } else { 
-        ofDrawEllipse(x+unitSize/2, y+unitSize/2, unitSize, unitSize);
-        ofDrawEllipse(x+unitSize/2+unitSize, y+unitSize/2+unitSize, unitSize, unitSize);
+
+// Create boundaries for the hidden objects.
+squareList[] = createHiddenObjectBoundaries({x0, y0, x1, y1},
+                                            {x2, y2, x3, y3},
+                                            {x4, y4, x5, y5},
+                                            {x6, y6, x7, y7});
+
+// Draw a grid of ellipses
+void drawGridOfElippses(){
+  // Loop through the width and height of the grid using
+  // unitSize as the grid interval.
+  for (float x = 0; x < screenWidth; x+=unitSize) {
+    for (float y = 0; y < screenHeight; y+=unitSize) {
+      // IF x AND y are EVEN
+      if (x/unitSize % 2 == 0 && y/unitSize % 2 == 0) {
+        // check if current position is within our hidden object boundaries
+        if ((x > [[x0]] AND x < [[x1]]) AND (y > [[y0]] && y < [[y1]]) OR
+            (x > [[x2]] AND x < [[x3]]) AND (y > [[y2]] && y < [[y3]]) OR
+            (x > [[x4]] AND x < [[x5]]) AND (y > [[y4]] && y < [[y5]]) OR
+            (x > [[x6]] AND x < [[x7]]) AND (y > [[y6]] && y < [[y7]])) {
+        // Draw rectangles to fill the space within hidden object boundaries
+          ofDrawRectangle(x + [[Unit Size]],
+                          y + [[Unit Size]],
+                          [[Unit Size]],
+                          [[Unit Size]]);
+          ofDrawRectangle(x, y, unitSize, unitSize);
+        } else {
+          // Otherwise, draw ellipses into space
+          ofDrawEllipse(x,
+                        y,
+                        [[Unit Size]],
+                        [[Unit Size]]);
+          ofDrawEllipse(x + [[Unit Size]] / 2 + [[Unit Size]],
+                        y + [[Unit Size]] / 2 + [[Unit Size]],
+                        [[Unit Size]],
+                        [[Unit Size]]);
+        }
       }
-    }      
+    }
   }
 }
