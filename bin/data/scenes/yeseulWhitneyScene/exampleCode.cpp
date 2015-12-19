461be4f01889822000a5c56cff1bd50142e5a1ee
Yeseul Song <pyeseul@gmail.com>
2015-12-18 23:14:01 -0500

    Update exampleCode.cpp

--- bin/data/scenes/yeseulWhitneyScene/exampleCode.cpp
+++ bin/data/scenes/yeseulWhitneyScene/exampleCode.cpp
@@ -1,18 +1,20 @@
 drawParticles() {
   // draw particles for the background and make the particles spin
   for (int r=0; r<width; r+=1) {
-    ofRotate([[spinSpeed]]*sin(r)*elapsedTime);
+    rotate([[spinSpeed]]*sin(r)*elapsedTime);
     for (int a=0; a<20; a+=1) {
-      ofRotate(360/20);
-      ofDrawCircle(0, r*10, 1);
+      rotate(360/20);
+      circle(0, r*10, 1);
     }
   }
 }
 
 drawDiffusion() {
   // draw expanding circle particles every [[diffusionInterval]] seconds
-  diffusionParticleSize = [[diffusionSize]];
   if ([[diffusionInterval]] <= totalTime - lastDiffusionTime) {
-    diffusion.draw();
-  }
+    for (int a=0; a<360; a+=10) {
+      rotate(10);
+      circle(0, y, [[diffusionSize]]);
+      }
+   }
 }
