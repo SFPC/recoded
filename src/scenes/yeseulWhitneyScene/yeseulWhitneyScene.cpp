3eeee598f69349662fe011c3050205f4c8a696c2
Yeseul Song <pyeseul@gmail.com>
2015-12-20 14:52:47 -0600

    Update yeseulWhitneyScene.cpp

--- src/scenes/yeseulWhitneyScene/yeseulWhitneyScene.cpp
+++ src/scenes/yeseulWhitneyScene/yeseulWhitneyScene.cpp
@@ -8,7 +8,7 @@ void yeseulWhitneyScene::setup(){
 
     parameters.add(spinSpeed.set("spinSpeed", 10, 10, 70));
     parameters.add(diffusionInterval.set("diffusionInterval", 5, 5, 10));
-    parameters.add(diffusionSize.set("diffusionSize", 1.3, 1, 3));
+    parameters.add(diffusionSize.set("diffusionSize", 0.7, 0.7, 2));
     
     lastDiffusionTime = 0;
     
