f081ec238be7fb185c014f2c244e6d35b71313be
mgs <mgsimpson@gmail.com>
2015-12-20 17:21:59 -0600

    improved rileyDescending pseudocode

--- bin/data/scenes/mgsRileyDescending/exampleCode.cpp
+++ bin/data/scenes/mgsRileyDescending/exampleCode.cpp
@@ -1,8 +1,18 @@
-void drawKinkedLines(width, height){
+setSpeed([[Speed]]);
+setFillColor(255); // 255 is for white
+setBackground(0); // 0 is for black
+setDepth([[Depth Amount]]);
+setColumnWidth([[Column Width]]);
+setRowHeight([[Row Height]]);
+
+void drawKinkedLines(){
   // Loop through the width and height of the grid
-  for(int columnBreak = 0; i < numberOfColumn; columnBreak++){
-      for(int rowNumber = 0; i < numberOfRows; rowNumber++){
-          drawKinkedLine(startX, startY, endX, endY, columnBreak, rowNumber)
+  for(int column = 0; i < numOfColumns; column += [[Column Width]]){
+      for(int row = 0; i < numOfRows; row += [[Row Height]]){
+        drawKinkedRileyLine(column,
+                            row,
+                            x*[[Column Width]]*[[Columns]],
+                            y*[[Rows]]*[[Row Height]]);
       }
   }
 }
