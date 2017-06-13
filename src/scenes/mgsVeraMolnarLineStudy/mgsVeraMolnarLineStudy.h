e089da3a849d5c42a3bfd893b6cbea65bc1ae291
mgs <mgsimpson@gmail.com>
2017-06-13 22:25:26 +0200

    Vera Sketch updates

--- src/scenes/mgsVeraMolnarLineStudy/mgsVeraMolnarLineStudy.h
+++ src/scenes/mgsVeraMolnarLineStudy/mgsVeraMolnarLineStudy.h
@@ -5,29 +5,52 @@
 
 class MolnarPainter {
  public:
+//<<<<<<< HEAD
+//  float x, y, startx, starty, lastx, lasty, angle, speed, thickness;
+//  bool connectedLines, invertColor, useColor;
+//  ofColor lineColor;
+//  int size, space, lineMax, lineCounter,shiftCounter,rowCounter, gridWidth, gridHeight, backgroundCounter;
+//=======
   float xOffset, yOffset, x, y, startx, starty, lastx, lasty, angle, speed, thickness;
   bool connectedLines, invertColor, useColor;
   ofColor lineColor;
   float size, space, lineMax, lineCounter,shiftCounter,rowCounter, gridWidth, gridHeight;
+  //>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
   ofColor stroke;
   
   void update() {
     glLineWidth(thickness);
     if(lineCounter < lineMax){
+//<<<<<<< HEAD
+//      int newx = ofRandom(startx+shiftCounter*(size+space), startx+shiftCounter*(size+space)+size);
+//      int newy = ofRandom(starty+rowCounter*(size+space), starty+rowCounter*(size+space)+size);
+//      /* if(invertColor){ */
+//      /*   ofClear(255); */
+//      /*   ofSetColor(0); */
+//      /*   ofNoFill(); */
+//      /* } else { */
+//      /*   ofClear(255); */
+//      /*   ofSetColor(0); */
+//      /*   ofNoFill(); */
+//      /*   ofSetColor(255); */
+//      /* } */
+//      if(!useColor){
+//        ofSetColor(backgroundCounter);
+//=======
       float newx = space+ofRandom(startx+shiftCounter*(size+space), startx+shiftCounter*(size+space)+size);
       float newy = space+ofRandom(starty+rowCounter*(size+space), starty+rowCounter*(size+space)+size);
       if(lineCounter == 0){
         ofSetColor(0);
+      } else if (!useColor){
+        if(connectedLines){
+          ofSetColor(255);
+        } else {
+          ofSetColor(255);
+        }
+        //>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
+      } else {
+        ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
       }
-//      } else if (!useColor){
-//        if(connectedLines){
-//          ofSetColor(255);
-//        } else {
-//          ofSetColor(255);
-//        }
-//      } else {
-//        ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
-//      }
       ofNoFill();
       ofDrawLine(x, y, newx, newy);
       x = newx;
@@ -35,6 +58,18 @@ class MolnarPainter {
       lineCounter++;
     } else {
       lineCounter = 0;
+//<<<<<<< HEAD
+//      if(shiftCounter < gridWidth) {
+//        shiftCounter++;
+//        lastx = x;
+//        lasty = y;
+//        x = startx+shiftCounter*(size+space);
+//        y = starty+rowCounter*(size+space);
+//        backgroundCounter = 0;
+//        if(connectedLines){
+//          if(!useColor){
+//            ofSetColor(255-backgroundCounter);
+//=======
       if(shiftCounter < floor(gridWidth)-1) {
         shiftCounter++;
         lastx = x;
@@ -44,18 +79,35 @@ class MolnarPainter {
         if(connectedLines){
           if(!useColor){
             ofSetColor(255);
+            //>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
           } else {
             ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
           }
           ofNoFill();
           ofDrawLine(lastx, lasty, x, y);
         }
+//<<<<<<< HEAD
+//        backgroundCounter = 0;
+//      } else {
+//        if(rowCounter < gridHeight){
+//=======
       } else {
         if(rowCounter < floor(gridHeight)-1){
+          //>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
           shiftCounter = 0;
           rowCounter++;
           lastx = x;
           lasty = y;
+//<<<<<<< HEAD
+//          x = startx+shiftCounter*(size+space);
+//          y = starty+rowCounter*(size+space);
+//          backgroundCounter = 0;
+//          if(connectedLines){
+//            if(!useColor){
+//              ofSetColor(255-backgroundCounter);
+//            } else {
+//              ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
+//=======
           //x = xOffset+startx+shiftCounter*(size+space);
           //y = yOffset+starty+rowCounter*(size+space);
           x = xOffset+startx+shiftCounter*(size+space);
@@ -65,6 +117,7 @@ class MolnarPainter {
               ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
             } else {
               ofSetColor(255);
+              //>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
             }
             ofNoFill();
             ofDrawLine(lastx, lasty, x, y);
@@ -74,11 +127,19 @@ class MolnarPainter {
           rowCounter = 0;
           lastx = x;
           lasty = y;
+//<<<<<<< HEAD
+//          x = startx;
+//          y = starty;
+//          if(connectedLines){
+//            if(!useColor){
+//              ofSetColor(255-backgroundCounter);
+//=======
           x = startx+xOffset;
           y = starty+yOffset;
           if(connectedLines){
             if(!useColor){
               ofSetColor(255);
+              //>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
             } else {
               ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
             }
@@ -101,6 +162,13 @@ public:
     void redrawF(float& i);
     void drawScene();
     void freshPaint();
+//<<<<<<< HEAD
+//    
+//    ofFbo frame;
+//    ofParameter<int> squareSize,numOfLines,gridHigh,gridWide,gridSpace;
+//    ofParameter<float> lineWidth;
+//    ofParameter<bool> connectedLinesP, invertedColor, useColorP;
+//=======
     void clearScreen(bool& i);
     void reset();
   
@@ -108,4 +176,5 @@ public:
     ofParameter<int> numOfLines;
     ofParameter<float> squareSize, gridSpace, lineWidth;
     ofParameter<bool> connectedLinesP, useColorP, animated, clearScreenBool;
+//>>>>>>> 708c864d367c75773ca8168ac961c5e7f26d86d5
 };
