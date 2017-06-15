4ae0cf2a2e2c9aade81f2437dc5df0e2a8344328
Yeseul Song <pyeseul@gmail.com>
2017-06-15 12:53:09 +0200

    change OF specific function names to general

--- bin/data/scenes/mgsRileyDiamonds/exampleCode.cpp
+++ bin/data/scenes/mgsRileyDiamonds/exampleCode.cpp
@@ -2,27 +2,27 @@ void draw(){
   clearScreen();
   for(float x = 0; x < width+gw; x += gw){
     for(float y = 0; y < height+gh; y += gh){
-      ofColor c = ofColor(ofRandom(0, [[Red Amount]]),
-                          ofRandom(0, [[Green Amount]]),
-                          ofRandom(0, [[Blue Amount]]),
+      color c = ofColor(random(0, [[Red Amount]]),
+                          random(0, [[Green Amount]]),
+                          random(0, [[Blue Amount]]),
                           255);
-      ofColor g = ofColor(ofRandom(0,255),
+      color g = color(random(0,255),
                           255);
       
       if([[grayscale]]){
-        ofSetColor(g);
-        ofFill();
+        setColor(g);
+        fill();
       } else {
-        ofSetColor(c);
-        ofFill();
+        setColor(c);
+        sill();
       }
-      ofBeginShape();
+      beginShape();
       vertex(x-gw, y+gh);
       vertex(x, y+gh);
       vertex(x+gw, y);
       vertex(x, y);
       vertex(x-gw, y+gh);
-      ofEndShape(true);
+      endShape(true);
     }
   }
 }
