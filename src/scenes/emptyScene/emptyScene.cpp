3cc3bccaf87d0fe623fdae72b04c5d41403332f6
Roy Macdonald <macdonald.roy@gmail.com>
2017-11-08 18:27:42 -0500

    Added instructions to emptyScene

--- src/scenes/emptyScene/emptyScene.cpp
+++ src/scenes/emptyScene/emptyScene.cpp
@@ -4,8 +4,13 @@
 void emptyScene::setup(){
   
 // setup pramaters
-//    param.set("param", 5, 0, 100);
-//    parameters.add(param);
+// if your original code use an ofxPanel instance dont use it here, instead
+// add your parameters to the "parameters" instance as follows.
+// param was declared in emptyScene.h
+    //parameters.add(param.set("param", 5, 0, 100));
+
+    setAuthor("Put Your Name Here");
+    setOriginalArtist("Put the original Artist's name here");
 
     loadCode("scenes/emptyScene/exampleCode.cpp");
 }
