16c7578c5684a70d0e1391bbba947420fd4a659d
Roy Macdonald <macdonald.roy@gmail.com>
2017-11-10 14:52:48 -0500

    Added EstherMolnarScene

---
+++ src/scenes/EstherMolnarScene/EstherMolnarScene.cpp
@@ -0,0 +1,23 @@
+
+#include "EstherMolnarScene.h"
+
+void EstherMolnarScene::setup(){
+  
+// setup pramaters
+// if your original code use an ofxPanel instance dont use it here, instead
+// add your parameters to the "parameters" instance as follows.
+// param was declared in EstherMolnarScene.h
+    //parameters.add(param.set("param", 5, 0, 100));
+
+    setAuthor("Put Your Name Here");
+    setOriginalArtist("Put the original Artist's name here");
+
+    loadCode("scenes/EstherMolnarScene/exampleCode.cpp");
+}
+
+void EstherMolnarScene::update(){
+    
+}
+
+void EstherMolnarScene::draw(){
+}
---
+++ src/scenes/EstherMolnarScene/EstherMolnarScene.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ofMain.h"
+#include "baseScene.h"
+
+class EstherMolnarScene : public baseScene {
+    
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    
+//    ofParameter<float> param;
+};
