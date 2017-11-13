9a35a6f954c1d306d70d88a81ad40175e93b5491
Ying Quan Tan <ying.quan.tan@gmail.com>
2017-11-13 16:57:27 -0500

    zz sushi scene

---
+++ bin/data/zzScenes/zzSushi/exampleCode.cpp
@@ -0,0 +1,4 @@
+// code goes here!
+
+//if you want to show a parameter's value, which highlights when it changes you need to enclose it's name in double square brackets.
+// for a parameter named myParam you shoudl put [[myParam]] in the pseudo code below.
